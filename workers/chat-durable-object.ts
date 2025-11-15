efa6cf422894f7e742cac1a2709f69723d5eb8e0
John Bjerke <bjerkeweb@gmail.com>
2025-11-15 11:59:53 -0600

    init, first stab at durable object config

---
+++ workers/chat-durable-object.ts
@@ -0,0 +1,47 @@
+import { DurableObject } from 'cloudflare:workers';
+
+export class ChatWebSocketServer extends DurableObject<Env> {
+  sessions: Map<WebSocket, { [key: string]: string }>;
+
+  constructor(ctx: DurableObjectState, env: Env) {
+    super(ctx, env);
+    this.sessions = new Map();
+
+    // wake up any hibernating WebSockets and place them back in sessions Map
+    this.ctx.getWebSockets().forEach((ws) => {
+      // check for previously attached state
+      const attachment = ws.deserializeAttachment();
+      if (attachment) {
+        this.sessions.set(ws, { ...attachment });
+      }
+    });
+
+    // sets an app level auto response that doesn't wake hibernated websockets
+    this.ctx.setWebSocketAutoResponse(
+      new WebSocketRequestResponsePair('ping', 'pong'),
+    );
+  }
+
+  async fetch(): Promise<Response> {
+    const webSocketPair = new WebSocketPair();
+    const [client, server] = Object.values(webSocketPair);
+
+    // calling this.ctx.acceptWebSocket() instead of ws.accept() informs the runtime
+    // that the socket is hibernatable and can be awoken/reconstrucred
+    this.ctx.acceptWebSocket(server);
+
+    // generate random ID
+    const id = crypto.randomUUID();
+
+    // attach id to ws and serialize it
+    server.serializeAttachment({ id });
+
+    // add to active sessions
+    this.sessions.set(server, { id });
+
+    return new Response(null, {
+      status: 101,
+      webSocket: client,
+    });
+  }
+}
